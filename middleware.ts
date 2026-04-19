326ec016cf2bfef7578174570b8ba9ddb717e85d
Dawid-Femin <d.femin@wp.pl>
2026-04-19 19:43:17 +0200

    fix: update middleware matcher to catch root path

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ import { routing } from './src/i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/', '/(pl|en)/:path*'],
+  matcher: ['/((?!_next|_vercel|.*\\..*).*)'],
 };
